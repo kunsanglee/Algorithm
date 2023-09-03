020b1aeef71fea285653e5402f752fcd058cbe3f
kunsanglee <gosmdochee2@gmail.com>
2023-09-03 16:40:11 +0900

    백준 10250, 2275

---
+++ practice/BOJ/Boj_10250_ACM호텔.java
@@ -0,0 +1,35 @@
+package practice.BOJ;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.StringTokenizer;
+
+public class Boj_10250_ACM호텔 {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int x = Integer.parseInt(br.readLine());
+        for (int i = 0; i < x; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            ArrayList<Integer> list = new ArrayList<>();
+            while (st.hasMoreTokens()) {
+                list.add(Integer.parseInt(st.nextToken()));
+            }
+
+            int height = list.get(0);
+            int num = list.get(2);
+
+            int room = num / height;
+            int floor = num % height;
+
+            if (floor == 0) {
+                System.out.println(height * 100 + room);
+            } else {
+                System.out.println(floor * 100 + room + 1);
+            }
+        }
+    }
+}
---
+++ practice/BOJ/Boj_2775_부녀회장이_될테야.java
@@ -0,0 +1,44 @@
+package practice.BOJ;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.StringTokenizer;
+
+public class Boj_2775_부녀회장이_될테야 {
+
+    /*
+     * 4층 | 1   6   21  56  126
+     * 3층 | 1   5   15  35  70
+     * 2층 | 1   4   10  20  35
+     * 1층 | 1   3   6   10  15
+     * 0층 | 1   2   3   4   5
+     * -----------------------
+     * 호  | 1   2   3   4   5
+     * */
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int testCase = Integer.parseInt(br.readLine());
+        for (int n = 0; n < testCase; n++) {
+            int floor = Integer.parseInt(br.readLine());
+            int room = Integer.parseInt(br.readLine());
+
+            int[][] arr = new int[floor + 1][room + 1];
+
+            for (int i = 0; i <= room; i++) {
+                arr[0][i] = i;
+            }
+
+            for (int i = 1; i <= floor; i++) {
+                int sum = 0;
+                for (int j = 0; j <= room; j++) {
+                    sum += arr[i - 1][j];
+                    arr[i][j] = sum;
+                }
+            }
+            System.out.println(arr[floor][room]);
+        }
+    }
+}
