ea856dec85df65e05765ac8e4694e8031e8aa95c
kunsanglee <gosmdochee2@gmail.com>
2024-08-06 17:16:42 +0900

    feat: 구현

--- practice/백준/Main.java
+++ practice/백준/Main.java
@@ -11,57 +11,53 @@ public class Main {
     static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
     public static void main(String[] args) throws IOException {
-        int t = Integer.parseInt(br.readLine());
-        for (int i = 0; i < t; i++) {
-            int n = Integer.parseInt(br.readLine());
-            String[] input = br.readLine().split(" ");
-            List<Integer> numbers = Arrays.stream(input)
-                    .map(Integer::parseInt)
-                    .collect(Collectors.toList());
-
-            // 필터링: 6명 이상의 선수가 있는 팀만 남김
-            Set<Integer> ok = numbers.stream()
-                    .filter(number -> Collections.frequency(numbers, number) >= 6)
-                    .collect(Collectors.toSet());
-
-            Map<Integer, List<Integer>> teamPositions = new HashMap<>();
-            for (int j = 0, k = 1; j < n; j++) {
-                int team = numbers.get(j);
-                if (ok.contains(team)) {
-                    teamPositions.putIfAbsent(team, new ArrayList<>());
-                    teamPositions.get(team).add(k++);
-                }
-            }
-
-            // 각 팀의 상위 4명의 점수 합산
-            Map<Integer, Integer> teamScores = new HashMap<>();
-            Map<Integer, Integer> teamFifthPositions = new HashMap<>();
-            for (Map.Entry<Integer, List<Integer>> entry : teamPositions.entrySet()) {
-                List<Integer> positions = entry.getValue();
-                Collections.sort(positions);
-                int team = entry.getKey();
-                int sum = positions.stream().limit(4).reduce(0, Integer::sum);
-                teamScores.put(team, sum);
-                teamFifthPositions.put(team, positions.get(4)); // 다섯 번째 주자의 순위 저장
-            }
-
-            // 우승팀 결정
-            int minScore = Integer.MAX_VALUE;
-            int winningTeam = -1;
-            for (int team : teamScores.keySet()) {
-                int score = teamScores.get(team);
-                if (score < minScore) {
-                    minScore = score;
-                    winningTeam = team;
-                } else if (score == minScore) {
-                    // 동점일 경우 다섯 번째 주자의 순위 비교
-                    if (teamFifthPositions.get(team) < teamFifthPositions.get(winningTeam)) {
-                        winningTeam = team;
-                    }
-                }
-            }
-
-            System.out.println(winningTeam);
+        int n = parseInput();
+        int count = parseInput();
+        List<Integer> positions = parseSplitToList(split(readLine(), " "));
+        // 연속된 가로등 간의 거리가 가장 먼것, 시작부터 처음 가로등, 마지막 가로등부터 끝 가로등 셋 중 가장 큰 값 찾기.
+        int max = positions.get(0);
+        for (int i = 0; i < positions.size() - 1; i++) {
+            int left = positions.get(i);
+            int right = positions.get(i + 1);
+            max = Math.max(max, calculate(right, left));
         }
+        max = Math.max(max, n - positions.get(positions.size() - 1));
+
+        System.out.println(max);
+    }
+
+    private static int calculate(int right, int left) {
+        int subtract = right - left;
+        if (subtract % 2 == 0) {
+            return subtract / 2;
+        }
+        return (subtract + 1) / 2;
+    }
+
+    private static String readLine() throws IOException {
+        return br.readLine();
+    }
+
+    private static int parseInput() throws IOException {
+        return Integer.parseInt(readLine());
+    }
+
+    private static int parseInt(String input) {
+        return Integer.parseInt(input);
+    }
+
+    private static String[] split(String input, String delimiter) {
+        return input.split(delimiter);
+    }
+
+    private static List<String> splitToList(String[] inputs) {
+        return Arrays.stream(inputs)
+                .collect(Collectors.toList());
+    }
+
+    private static List<Integer> parseSplitToList(String[] inputs) {
+        return Arrays.stream(inputs)
+                .map(Integer::parseInt)
+                .collect(Collectors.toList());
     }
 }
---
+++ practice/백준/구현/silver/크로스컨트리_9017/Main.java
@@ -0,0 +1,73 @@
+package practice.백준.구현.silver.크로스컨트리_9017;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+public class Main {
+
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+    public static void main(String[] args) throws IOException {
+        int t = Integer.parseInt(br.readLine());
+        for (int i = 0; i < t; i++) {
+            int n = Integer.parseInt(br.readLine());
+            String[] input = br.readLine().split(" ");
+            List<Integer> numbers = Arrays.stream(input)
+                    .map(Integer::parseInt)
+                    .collect(Collectors.toList());
+
+            // 필터링: 6명 이상의 선수가 있는 팀만 남김
+            Set<Integer> ok = numbers.stream()
+                    .filter(number -> Collections.frequency(numbers, number) >= 6)
+                    .collect(Collectors.toSet());
+
+            Map<Integer, List<Integer>> teamPositions = new HashMap<>();
+            for (int j = 0, k = 1; j < n; j++) {
+                int team = numbers.get(j);
+                if (ok.contains(team)) {
+                    teamPositions.putIfAbsent(team, new ArrayList<>());
+                    teamPositions.get(team).add(k++);
+                }
+            }
+
+            // 각 팀의 상위 4명의 점수 합산
+            Map<Integer, Integer> teamScores = new HashMap<>();
+            Map<Integer, Integer> teamFifthPositions = new HashMap<>();
+            for (Map.Entry<Integer, List<Integer>> entry : teamPositions.entrySet()) {
+                List<Integer> positions = entry.getValue();
+                Collections.sort(positions);
+                int team = entry.getKey();
+                int sum = positions.stream().limit(4).reduce(0, Integer::sum);
+                teamScores.put(team, sum);
+                teamFifthPositions.put(team, positions.get(4)); // 다섯 번째 주자의 순위 저장
+            }
+
+            // 우승팀 결정
+            int minScore = Integer.MAX_VALUE;
+            int winningTeam = -1;
+            for (int team : teamScores.keySet()) {
+                int score = teamScores.get(team);
+                if (score < minScore) {
+                    minScore = score;
+                    winningTeam = team;
+                } else if (score == minScore) {
+                    // 동점일 경우 다섯 번째 주자의 순위 비교
+                    if (teamFifthPositions.get(team) < teamFifthPositions.get(winningTeam)) {
+                        winningTeam = team;
+                    }
+                }
+            }
+
+            System.out.println(winningTeam);
+        }
+    }
+}
